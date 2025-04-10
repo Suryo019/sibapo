6f67c51ef471f4b8d5e0ae82eec373c00230df86
aryanesta <aryanesta2@gmail.com>
2025-04-10 14:59:39 +0700

    update front-end

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+<<<<<<< Updated upstream
 Route::get('/dashboard', function () {
     return view('admin.admin-dashboard');
 });
@@ -13,6 +14,18 @@ Route::get('/dashboard', function () {
 Route::get('/disperindag', function () {
     return view('admin.admin-disperindag');
 });
+=======
+Route::resource('disperindag', DisperindagController::class)->names([
+    'index' => 'disperindag.index',
+    'create' => 'disperindag.create',
+    'store' => 'disperindag.store',
+    'show' => 'disperindag.show',
+    'edit' => 'disperindag.edit',
+    'update' => 'disperindag.update',
+    'destroy' => 'disperindag.destroy',
+]);
+Route::get('disperindag/detail', [DisperindagController::class, 'detail'])->name('disperindag.detail');
+>>>>>>> Stashed changes
 
 Route::get('/dtphp', function () {
     return view('admin.admin-dtphp');
